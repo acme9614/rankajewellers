276db05cbd27b6bc01d8c736b1239d4803bdb571
prajakta_a_kadam <prajaktakadam63341@gmail.com>
2025-08-26 12:59:06 +0530

    catalogues & quick purchase removed

--- script.js
+++ script.js
@@ -135,17 +135,17 @@ const tabs = [
     action: () => navigateToScheme(),
   },
 
-    {
-    label: "Catalogues",
-    icon: "https://cdn-icons-png.flaticon.com/128/29/29341.png",
-    action: () => navigateToCatalogues(),
-  },
+  //   {
+  //   label: "Catalogues",
+  //   icon: "https://cdn-icons-png.flaticon.com/128/29/29341.png",
+  //   action: () => navigateToCatalogues(),
+  // },
 
-      {
-    label: "Quick Purchase",
-    icon: "https://cdn-icons-png.flaticon.com/128/1019/1019709.png",
-    action: () => navigateToQuickPurchase(),
-  },
+  //     {
+  //   label: "Quick Purchase",
+  //   icon: "https://cdn-icons-png.flaticon.com/128/1019/1019709.png",
+  //   action: () => navigateToQuickPurchase(),
+  // },
 
   
 
